b559df224f44f0e9008c936bff85eae0e5d5dc05
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2023-11-26 09:56:05 +0700

    testing form changes

--- src/app/components/Form/form.tsx
+++ src/app/components/Form/form.tsx
@@ -1,8 +1,22 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import Script from 'next/script'
 
 export default function Form() {
+  return (
+    <>
+      <div className="lg:w-[65%]">
+        <div id="ff-compose"></div>
+        <Script
+          id="form"
+          src="https://formfacade.com/include/110808910195665411918/form/1FAIpQLSfRKdf-wyQIzzq362Wo6ZpOmujIDSCBhcc0Gd1fY2UOli8k1w/classic.js?div=ff-compose"
+        ></Script>
+      </div>
+    </>
+  )
+}
+
+/*export default function Form() {
   const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
@@ -18,11 +32,19 @@ export default function Form() {
             async
             defer
             src="https://formfacade.com/include/110808910195665411918/form/1FAIpQLSfRKdf-wyQIzzq362Wo6ZpOmujIDSCBhcc0Gd1fY2UOli8k1w/classic.js?div=ff-compose"
-          ></script>
+          ></scrip>
         </div>
       ) : (
         ''
       )}
     </>
   )
-}
+}*/
+
+/* <Script
+async
+defer
+src="https://formfacade.com/include/110808910195665411918/form/1FAIpQLSfRKdf-wyQIzzq362Wo6ZpOmujIDSCBhcc0Gd1fY2UOli8k1w/classic.js?div=ff-compose"
+></Script>
+
+*/
