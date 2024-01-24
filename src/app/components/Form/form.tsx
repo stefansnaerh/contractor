0166fde1bc94949a92351a3b1f634bf869a83bbb
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2024-01-24 22:26:13 +0000

    testing

--- src/app/components/Form/form.tsx
+++ src/app/components/Form/form.tsx
@@ -1,19 +1,19 @@
-'use client'
+// 'use client'
 
-import Script from 'next/script'
+// import Script from 'next/script'
 
-export default function Form() {
-  return (
-    <>
-      {' '}
-      <iframe
-        data-tally-src="https://tally.so/embed/mBxAN4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-        loading="lazy"
-        width="100%"
-        height="1"
-        title="Fá tilboð"
-      ></iframe>
-      <Script id="tally-js" src="https://tally.so/widgets/embed.js" />
-    </>
-  )
-}
+// export default function Form() {
+//   return (
+//     <>
+//       {' '}
+//       <iframe
+//         data-tally-src="https://tally.so/embed/mBxAN4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+//         loading="lazy"
+//         width="100%"
+//         height="1"
+//         title="Fá tilboð"
+//       ></iframe>
+//       <Script id="tally-js" src="https://tally.so/widgets/embed.js" />
+//     </>
+//   )
+// }
